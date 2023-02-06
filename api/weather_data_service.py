910479b794acf2dfc1efe021307ff37914653ece
davidayomide <ayomideshoyemi@yahoo.com>
2023-02-06 12:05:15 +0100

    fin

--- api/weather_data_service.py
+++ api/weather_data_service.py
@@ -1,10 +1,10 @@
 import requests
 
-def get_weather_data(location):
-    api_key = 
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+def get_weather_data(location, api_key):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(location, api_key)
+    response = requests.get(api_url)
+    return response.json()
+
+weather_data = get_weather_data("London", "7f3fa893d85a38a0a4d9b468bb423365")
+print(weather_data)
+
